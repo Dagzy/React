d6d6b7c8a84cf41053089c74b560a1a4af908f6c
Dagzy <quincye.williams@gmail.com>
2018-03-19 13:23:49 -0400

    QW added files

---
+++ testy.js
@@ -0,0 +1,9 @@
+const colors = ['green', 'yellow', 'blue', 'red']
+let simon = [];
+
+for (let i = 0; i < colors.length; i++) {
+    const element = colors[i];
+    console.log(element)
+}
+
+let randomizer = '' 
